07035708a35eb06b4f8d14ff15f2340736fa2ca2
Pontus Olausson <pontusolausson@hotmail.com>
2020-06-25 12:44:59 +0200

    Small changes to host on GCE

--- server/src/database.js
+++ server/src/database.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 
 let connection = mysql.createConnection({
-  // host: '35.228.147.168',
+  host: '35.228.147.168',
   user: 'TestUser',
   password: 'securePassword',
   database: 'dialogdata_db',
-  socketPath: '/cloudsql/dialogdata:europe-north1:dialogdata-mysql'
+  // socketPath: '/cloudsql/dialogdata:europe-north1:dialogdata-mysql'
 });
 
 connection.connect((err) => {
