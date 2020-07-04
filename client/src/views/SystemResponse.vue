6f4f1fee333640ea4be3a25fd2160bdc5a3ab2da
Pontus Olausson <pontusolausson@hotmail.com>
2020-07-04 20:51:48 +0200

    Initial changes to SystemResponseView

--- client/src/views/SystemResponse.vue
+++ client/src/views/SystemResponse.vue
@@ -1,31 +1,42 @@
 
 <template>
-  <div class="text-box col-md-4 col-md-offset-4" style="text-align: center" v-if="dialogue">
-    <h1>Fortsätt på denna dialog!</h1>
-    <h4>Du har nu rollen som busschafför. Svara användaren genom att välja det av svaren som passar bäst, givet följande dialog.</h4>
-    <div class="well">
-      <div class="dialogueTurn" v-for="utterance in dialogue.utterances" :key="utterance.uttrID">
-        <h4 class="userUtterance">
-          Användare: {{ utterance.uttr }}
+  <div class="container-fluid" v-if="dialogue">
+    <div class="row" style="text-align: center">
+      <div class="col-md-4 col-md-offset-4">
+        <h1>Fortsätt på denna dialog!</h1>
+        <h4>
+          Du har nu rollen som busschafför.
+          Svara användaren genom att välja det av svaren som passar bäst, givet följande dialog.
         </h4>
-        <h4 v-if="utterance.systemResponseText" class="systemUtterance">
-          Busschaufför: {{ utterance.systemResponseText }}
-        </h4>
+        <div class="well">
+          <div class="dialogueTurn" v-for="utterance in dialogue.utterances"
+           :key="utterance.uttrID">
+            <h4 class="userUtterance">
+              Användare: {{ utterance.uttr }}
+            </h4>
+            <h4 v-if="utterance.systemResponseText" class="systemUtterance">
+              Busschaufför: {{ utterance.systemResponseText }}
+            </h4>
+          </div>
+        </div>
+        <div class="flexBoxLogin">
+          <button type="button" class="btn btn-info" data-toggle="modal" data-target="#infoModal">
+            ?
+          </button>
+          <h3>Vilket av följande alternativ passar bäst?</h3>
+        </div>
       </div>
     </div>
-    <div>
-      <div class="flexBoxLogin">
-        <button type="button" class="btn btn-info" data-toggle="modal" data-target="#infoModal">
-          ?
-        </button>
-        <h3>Vilket av följande alternativ passar bäst?</h3>
-      </div>
-      <div class="well" v-for="template in templates" v-bind:key="template.templateID"
-       @click="pickSystemResponse(template.templateID)">
-        <h2>{{ template.template }}</h2>
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        <div class="well" v-for="template in templates" v-bind:key="template.templateID"
+         @click="pickSystemResponse(template.templateID)">
+          <h2>{{ template.template }}</h2>
+        </div>
       </div>
     </div>
-    <div class="modal fade" id="infoModal" tabindex="-1" role="dialog" aria-labelledby="infoModalLabel" aria-hidden="true">
+    <div class="modal fade" id="infoModal" tabindex="-1" role="dialog"
+     aria-labelledby="infoModalLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
@@ -35,7 +46,7 @@
             </button>
           </div>
           <div class="modal-body">
-            Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan. Detta är förklaringen för sidan.
+            Detta är förklaringen för sidan.
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-Primary" data-dismiss="modal">Stäng</button>
@@ -45,8 +56,8 @@
     </div>
   </div>
   <div class="text-box col-md-4 col-md-offset-4" style="text-align: center" v-else>
-    <h2>Seems like there is currently no utterance that you pick a response to :(</h2>
-    <h3>Try generating some new instead utterances instead!</h3>
+    <h2>Seems like there is currently no utterance for you to respond to :(</h2>
+    <h3>Try generating some new instead!</h3>
   </div>
 </template>
 
